3b3ea35cbbc932e0b1475ab07286e22123f18f01
mukremin1 <mukremin.cakmak.da@gmail.com>
2025-12-09 13:15:17 +0300

    fix: remove BOM from files to avoid Vite JSON.parse errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-﻿import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { VitePWA } from "vite-plugin-pwa";
